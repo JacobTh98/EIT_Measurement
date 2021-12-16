314c68d3f0b8add235659f3ac23d8df2e7360282
JaTh <jacob.thoenes@igp.fraunhofer.de>
2021-12-16 22:07:14 +0100

    update

--- .ipynb_checkpoints/functions-checkpoint.py
+++ .ipynb_checkpoints/functions-checkpoint.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import cv2 as cv
 import matplotlib.pyplot as plt
+from PIL import Image
 """
 Um dieses Modul nach einer Veränderung zu verwenden, muss der Kernel neu gestartet werden.
 """
@@ -50,29 +51,61 @@ def export_xlsx(A,name ='undefined',transpose=True):
     
     print('Exported as:',name+str('.xlsx'))
 ###--------------------------------------------------------
-
-def gen_ground_truth_pic(body,r,α):
+def gen_ground_truth_pic(objct , r , α , clockdirection=False, save_img = False):
     """
-    Input: body ... 'rectangle','square', 'triangle'
-           r    ... Radius vom Mittelpunkt
-           α    ... Winkel von positiver x-Achse (im Uhrzeigersinn)
+    Input: objct ...'rectangle','circle','triangle'
+           r    ... Radius 0...2π [Rad] vom Mittelpunkt
+           α    ... Winkel [°] von positiver x-Achse (default: gegen den Uhrzeigersinn)
+           
+           -> Für mehr Objekte Arrays für objct,r,α übergeben
     Return:
            Bild des Gegenstandes, positioniert im Einheitskreis.
+           
+           Change rotation cercle!!!
     """
     IMG=np.zeros((1000,1000))
-    x_0 = y_0 =500 #Verschiebung Nullpunkt Koordinatensystem
-    x = round(r*np.cos(α))
-    y = round(r*np.sin(α))
-    print('x:',x)
-    print('y:',y)
-    
-    #              x,y      r     color.   strichdicke 
     cv.circle(IMG,(500,500),500,(255,255,0),1) #Rand
-    if body == 'circle':
-        cv.circle(IMG,(x_0+x,y_0+y),100,(255,0,0),-1)
-    if body == 'rectangle':
-        abw=100
-        cv.rectangle(IMG,(x_0-abw+x,y_0-abw+y),(x_0+abw+x,y_0+abw+y),(255,0,0),-1)
+    r_old = r
+    α_old = α
+    if type(r) == int:
+        r = np.array([r])
+        α = np.array([α])
+        objct = np.array([objct])
+        end = 1
+    else:
+        r = np.array(r)
+        α = np.array(α)
+        objct = np.array(objct)    
+        end = len(r)
+    
+    for cnt in range(end):
+        if clockdirection:
+            α[cnt] = α[cnt]*(-1)# Grad in Radiant
+        else:
+            α[cnt] = α[cnt]     # Grad in Radiant
+        if r[cnt]>100:
+            print('r ist zu groß; Skalierbar von 0...100%')
+            return
+        r[cnt] = abs(r[cnt]*4)
+        x_0 = y_0 = 500 #Verschiebung Nullpunkt Koordinatensystem
+        x = round(r[cnt]*np.cos(α[cnt]))
+        y = round(r[cnt]*np.sin(α[cnt]))
+        if objct[cnt] == 'circle':
+            cv.circle(IMG,(x_0+x,y_0+y),100,(255,0,0),-1)
+        if objct[cnt] == 'rectangle':
+            abw=100
+            cv.rectangle(IMG,(x_0-abw+x,y_0-abw+y),(x_0+abw+x,y_0+abw+y),(255,0,0),-1)
+        if objct[cnt] == 'triangle':
+            pt1 = (500+x, 400+y)
+            pt2 = (600+x, 600+y)
+            pt3 = (400+x, 600+y)
+            tri_edges = np.array( [pt1, pt2, pt3] )
+            cv.drawContours(IMG, [tri_edges], 0, (255,0,0), -1)
+    
+    if save_img:
+        im = Image.fromarray(IMG)
+        im = im.convert("L")
+        im.save(str(objct)+str(r_old)+str(α_old)+".jpeg")
+        print('Bild gespeichert')
     return IMG
-
 ###--------------------------------------------------------
